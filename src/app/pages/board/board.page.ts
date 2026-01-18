45346572a13fbc5756243a4481b63802549db6bb
zborja <60552547+zborja@users.noreply.github.com>
2026-01-18 13:29:33 -0800

    Board: display mock intentions

--- src/app/pages/board/board.page.ts
+++ src/app/pages/board/board.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonButton } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButton, IonList, IonItem, IonLabel } from '@ionic/angular/standalone';
 import { RouterModule } from '@angular/router';
 
 @Component({
@@ -9,7 +9,7 @@ import { RouterModule } from '@angular/router';
   templateUrl: './board.page.html',
   styleUrls: ['./board.page.scss'],
   standalone: true,
-  imports: [RouterModule, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButton]
+  imports: [RouterModule, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButton, IonList, IonItem, IonLabel]
 })
 export class BoardPage implements OnInit {
 
@@ -17,5 +17,11 @@ export class BoardPage implements OnInit {
 
   ngOnInit() {
   }
+
+  intentions = [
+    { id: 1, title: 'move my body with care', imageUrl: ''},
+    { id: 2, title: 'say yes to travel', imageUrl: ''},
+    { id: 3, title: 'feel at home with myself', imageUrl: ''}
+  ]
 
 }
